0b472009388e1e633f83a23b43f6fe42f0279560
remipoulenard <remi.poulenard@epitech.eu>
2021-02-09 16:38:28 +0100

    [Add] exercise 1

---
+++ exercises/ex01/workshopexo1/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    Exercice 1
+    <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ exercises/ex01/workshopexo1/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container class="grey lighten-5" fill-height fluid>
+    <v-row>
+      <v-col>
+        <v-card class="photocard" color="black"></v-card>
+      </v-col>
+      <v-col>
+        <v-card class="photocard" color="grey"></v-card>
+      </v-col>
+      <v-col>
+        <v-card class="photocard" color="primary"> </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card class="photocard" color="red"> </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="photocard" color="yellow"> </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="photocard" color="black"> </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card class="photocard" color="green"> </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="photocard" color="grey"> </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="photocard" color="purple"> </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({}),
+};
+</script>
+
+<style>
+
+.photocard {
+  height : 200px;
+}
+
+</style>
