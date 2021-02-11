1f0ff5add9d69f8670b040ada3c253845b57aa3a
remipoulenard <remi.poulenard@epitech.eu>
2021-02-11 19:11:02 +0100

    [remove] useless comment

--- workshop-demo/src/views/Dogs.vue
+++ workshop-demo/src/views/Dogs.vue
@@ -1,22 +1,4 @@
 <template>
-  <!-- <v-container pt-16>
-    <v-row>
-      <v-col align="center">
-        <v-card max-width="600px" elevation="24">
-          <v-carousel>
-            <v-carousel-item
-              v-for="(url, i) in getDogsPhotosUrl"
-              :key="i"
-              reverse-transition="fade-transition"
-              transition="fade-transition"
-            >
-              <v-img :src="url" width="600px" min-height="500"></v-img>
-            </v-carousel-item>
-          </v-carousel>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container> -->
   <v-row>
     <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
       <v-img :src="getDogsPhotosUrl[n]" aspect-ratio="1" class="grey lighten-2">
